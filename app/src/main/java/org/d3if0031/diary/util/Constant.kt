75b81e3dda30b61f301870078daf59c9498800af
romegaarini <romega.purba123@gmail.com>
2022-05-11 20:30:17 +0700

    Intial Commit

---
+++ app/src/main/java/org/d3if0031/diary/util/Constant.kt
@@ -0,0 +1,10 @@
+package org.d3if0031.diary.util
+
+object Constant {
+    const val MINUTE_IN_SECONDS = 60
+    const val HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60
+    const val DAY_IN_SECONDS = HOUR_IN_SECONDS * 24
+    const val WEEK_IN_SECONDS = DAY_IN_SECONDS * 7
+    const val MONTH_IN_SECONDS = WEEK_IN_SECONDS * 4
+    const val YEAR_IN_SECONDS = MONTH_IN_SECONDS * 12
+}
